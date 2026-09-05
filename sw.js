c886617efa1893c3ddb4d18c96a36f75d47b2ab3
shafiaakhter17-maker <shafiaakhter17@gmail.com>
2026-09-05 20:34:35 +0530

    Update service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-const CACHE_NAME = "lumitales-v2";
+const CACHE_NAME = "lumitales-v3";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -14,18 +16,51 @@ self.addEventListener("install", event => {
   );
 
   self.skipWaiting();
+
 });
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    self.clients.claim()
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+
+    })
+
   );
+
+  self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+
+    fetch(event.request)
+      .then(response => {
+
+        const copy=response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request,copy);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+
+        return caches.match(event.request);
+
+      })
+
   );
+
 });
